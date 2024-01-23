567d61b6543555e45d0365d6ab39e6c6f009fcf3
deni sugiarto <sugiarto.deni@gmail.com>
2024-01-23 09:12:46 +0700

    first commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <MainLayout>
+        <h1>Index</h1>
+        <div>The message is {{ message }}</div>
+    </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from "../../Layouts/MainLayout.vue";
+defineProps({
+    message: String,
+});
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,10 @@
+<template>
+    <MainLayout>
+        <h1>Show</h1>
+    </MainLayout>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import MainLayout from "../../Layouts/MainLayout.vue";
+</script>
