2f55704d33098ef7b1a47c8af42dcfe6d7a3e653
deni sugiarto <sugiarto.deni@gmail.com>
2024-01-29 13:08:39 +0700

    feat:add inertia middleware

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,13 +1,30 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/hello">Show Page</Link>
-    <div>The Page with time {{ timer }}</div>
+    <!-- <div>The Page with time {{ timer }}</div> -->
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>
+    <!-- <div>{{ y }}</div> -->
+    <!-- <p>{{ JSON.stringify(page, null, 2) }}</p> -->
     <slot>Default</slot>
 </template>
 <script setup>
-import { Link } from "@inertiajs/vue3";
-import { ref } from "vue";
-const timer = ref(0);
+import { computed, ref } from "vue";
+import { Link, usePage } from "@inertiajs/vue3";
+// import { ref } from "vue";
+const page = usePage();
+const flashSuccess = computed(() => page.props.flash.success);
+// const timer = ref(0);
+// const x = ref(0);
+// const y = computed(() => x.value * 2);
 
-setInterval(() => timer.value++, 1000);
+// setInterval(() => timer.value++, 1000);
 </script>
+
+<style scoped>
+.success {
+    background-color: green;
+    color: white;
+}
+</style>
